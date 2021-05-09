313ae24bfe86c75e4b7a268b718cca2cf9785bb6
andreisemianiuk <semeniukandrei677@gmail.com>
2021-05-09 19:33:00 +0300

    added middleware for redux-thunk

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,13 +1,14 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import { tasksReducer } from './tasks-reducer'
 import { todolistReducer } from './todolist-reducer'
 
 const rootReducer = combineReducers({
   todolists: todolistReducer,
-  tasks: tasksReducer
+  tasks: tasksReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type RootStateType = ReturnType<typeof rootReducer>
 
