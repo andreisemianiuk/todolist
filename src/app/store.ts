25029593b8a48ecf8e8876fe4821efc2a2fa3cae
andreisemianiuk <semeniukandrei677@gmail.com>
2021-05-23 23:17:47 +0300

    added app-reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { tasksReducer } from '../features/tasks-reducer'
 import { todolistReducer } from '../features/todolist-reducer'
+import { appReducer } from './app-reducer'
 
 const rootReducer = combineReducers({
   todolists: todolistReducer,
   tasks: tasksReducer,
+  app: appReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
